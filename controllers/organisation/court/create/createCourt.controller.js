2c6da886b47b492e1d72d8539cb4da662a589e6b
Aritra Naharay <aritranaharay@gmail.com>
2026-06-14 20:31:39 +0530

    added valiation for courtCreation

--- controllers/organisation/court/create/createCourt.controller.js
+++ controllers/organisation/court/create/createCourt.controller.js
@@ -1,14 +1,20 @@
 const Court = require("@/models/organisation/court.model");
-
+const { createSuccessResponse, createErrorResponse } = require('@/utils/helpers/errorFormat/errorFormatter');
+const STATUS_CODES = require('@/utils/helpers/statusCodes.helper')
 const CreateCourtController = async (request, response) => {
     try {
         const { orgId, venueId, sportId, courtNumber, courtName, isMultipurpose } = request.body;
         const userId = request.user.id;
-
+        const existingCourt = await Court.findOne({ orgId, venueId, sportId: { $in: [sportId] }, courtNumber, courtName, status: "active" })
+        if (existingCourt) {
+            return response.status(STATUS_CODES.BAD_REQUEST).json(
+                createErrorResponse(STATUS_CODES.BAD_REQUEST, "popup", "Court with Name and Number already Exists for that sport.")
+            );
+        }
         const newCourt = await Court.create({
             orgId,
             venueId,
-            sportId,
+            sportId: sportId,
             courtNumber,
             courtName,
             isMultipurpose,
