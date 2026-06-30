d661d3297876153149d1d46f96a864fb59f5c86e
Aritra Naharay <aritranaharay@gmail.com>
2026-06-30 16:01:04 +0530

    removed console

--- utils/connections/jwt/getToken.jwt.js
+++ utils/connections/jwt/getToken.jwt.js
@@ -1,6 +1,5 @@
 const GetToken = (request) => {
     const authHeader = request.headers["authorization"];
-    console.log("authHeader", authHeader)
     const token = authHeader && authHeader.split(" ")[1];
 
     return token;
